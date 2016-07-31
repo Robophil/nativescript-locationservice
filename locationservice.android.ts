e7edbe2f6b645c11d0489f3720563b73c7581dc6
Robophil <oghenerobo@gmail.com>
2016-07-31 06:46:41 +0100

    Done, first release

--- locationservice.android.ts
+++ locationservice.android.ts
@@ -1,5 +1,18 @@
+declare var android: any;
+declare var com: any;
+declare var java: any;
+
 let app = require("application");
 
 export function startService(uuid: string){
-    
+    if (app.android) {
+        let Intent = android.content.Intent;
+        let intent = new Intent(app.android.context, com.github.robophil.location_service.LocationService.class);
+        intent.putExtra("uuid", uuid);
+
+        //start service
+        app.android.context.startService(intent);
+    }else{
+        console.log("Android only service.....intrested in IOS premium ?? ;)")
+    }
 }
